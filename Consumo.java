8aedc86fd0462551ee96d39cd0475761a7e128d3
spptunes <113805781+spptunes@users.noreply.github.com>
2022-10-04 14:22:00 -0400

    Add files via upload
    Classe com os atributos (classe modelos)

---
+++ Consumo.java
@@ -0,0 +1,45 @@
+package Model;
+
+public class Consumo {
+	private int codigo;
+	private String descricao;
+	private int quantidade;
+	private double valorUnitario;
+	
+	public Consumo() {
+		
+	}
+	
+	public Consumo(int codigo, String descricao, int quantidade, double valorUnitario) {
+		super();
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.valorUnitario = valorUnitario;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}	
+}
